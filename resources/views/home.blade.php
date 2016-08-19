737728baa98691e31a66de05dbb88852b305c7a6
joesanel <eliojavier86@gmail.com>
2016-08-19 00:00:23 -0400

    primer commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,37 @@
+
+		<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Contador de likes
+	</title>
+
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="js/jquery.counter-analog.css"/>
+	<link rel="stylesheet" type="text/css" href="js/jquery.counter-analog2.css"/>
+	<link rel="stylesheet" type="text/css" href="js/jquery.counter-analog3.css"/>
+
+	<link rel="stylesheet" href="css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.js"></script>
+	<script src="js/jquery.counter.js"></script>
+</head>
+
+<body>
+hola mundo
+
+<!--
+  Below we include the Login Button social plugin. This button uses
+  the JavaScript SDK to present a graphical Login button that triggers
+  the FB.login() function when clicked.
+-->
+
+<fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+</fb:login-button>
+
+<div id="status">
+</div>
+</body>
+</html>
+
